a6661c00b101b46424084fea970ab751246f8f50
wusir57 <13917789448@163.com>
2023-12-10 21:59:49 +0800

    BYD,数据库拖了好久，wuwuuw

--- src/main/java/com/restaurant/restaurant/mapper/MessageMapper.java
+++ src/main/java/com/restaurant/restaurant/mapper/MessageMapper.java
@@ -1,4 +1,10 @@
 package com.restaurant.restaurant.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface MessageMapper {
+    List<MessageMapper> selectAll();
+    MessageMapper selectById(@Param("id") int id);
 }
--- src/main/java/com/restaurant/restaurant/mapper/VoteMapper.java
+++ src/main/java/com/restaurant/restaurant/mapper/VoteMapper.java
@@ -1,4 +1,10 @@
 package com.restaurant.restaurant.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface VoteMapper {
+    List<VoteMapper> selectAll();
+    VoteMapper selectById(@Param("id") int id);
 }
