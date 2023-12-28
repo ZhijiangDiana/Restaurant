79866dff03551c4532baf2fb7a5b708ca2278adc
cj <2477835558@qq.com>
2023-12-29 02:06:16 +0800

    获取userId

---
+++ src/main/java/com/restaurant/restaurant/controller/login/GetUserId.java
@@ -0,0 +1,21 @@
+package com.restaurant.restaurant.controller.login;
+
+import com.restaurant.restaurant.pojo.entity.User;
+import com.restaurant.restaurant.utils.FrontEndUtils;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+
+@WebServlet("/getUserId")
+public class GetUserId extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        response.getWriter().print(FrontEndUtils.objectToBody(null,"0",user.getUserId()));
+    }
+}
